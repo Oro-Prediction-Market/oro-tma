1ef9a6544059119136743d1e55de48073598121d
SonamTenzin7 <sonamtenzey0@gmail.com>
2026-07-01 00:41:16 +0600

    minor changes

--- src/pages/TmaLeaderboardPage.tsx
+++ src/pages/TmaLeaderboardPage.tsx
@@ -1613,7 +1613,7 @@ export const TmaLeaderboardPage: FC = () => {
                   fontWeight: 600,
                 }}
               >
-                15+ predictions needed to appear on the board
+                10+ predictions needed to appear on the board
               </p>
             </div>
 
